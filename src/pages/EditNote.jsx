b54e9895af1da03704242e6f498fab8006091e88
Abiodun <abiodunhodonu@gmail.com>
2023-07-13 23:05:41 +0100

    edit note page built

--- src/pages/EditNote.jsx
+++ src/pages/EditNote.jsx
@@ -1,10 +1,21 @@
+import { Link } from 'react-router-dom'
+import { IoIosArrowBack } from 'react-icons/io'
+import { RiDeleteBin6Line } from 'react-icons/ri'
 
 
 const EditNote = () => {
   return (
-    <div>
-      Edit Note
-    </div>
+    <section>
+      <header className="create-note__header">
+        <Link to="/" className='btn'><IoIosArrowBack /></Link>
+        <button className="btn lg primary">Save</button>
+        <button className="btn lg danger"><RiDeleteBin6Line /> </button>
+      </header>
+      <form className="create-note__form">
+        <input type="text" placeholder='title...' autoFocus />
+        <textarea rows="30" placeholder='write everthing here'></textarea>
+      </form>
+    </section>
   )
 }
 
