a338d8c0623753383eadd3100962a7d7119da2d3
Abiodun <abiodunhodonu@gmail.com>
2023-07-13 22:01:53 +0100

    created note page

---
+++ src/components/NoteItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NoteItem = () => {
+  return (
+    <div>
+      note items
+    </div>
+  )
+}
+
+export default NoteItem
--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { CiSearch } from 'react-icons/ci'
+import { BsPlusLg } from 'react-icons/bs'
+import dummyNotes from '../dummy_notes'
+import { Link } from 'react-router-dom'
+import NoteItem from '../components/NoteItem'
 
 const Notes = () => {
   return (
-    <div>
-      Note Homepage
-    </div>
+    <section>
+      <header className='notes__header'>
+        <h2>Notes</h2>
+        {/* <input type="text" autoFocus placeholder='Keyword...' /> */}
+        <button className='btn'><CiSearch /></button>
+      </header>
+      <div className="notes__container">
+    {
+      //note props
+      dummyNotes.map(note => <NoteItem key={note.id} note={note}/>)
+    }
+      </div>
+      <Link><BsPlusLg className='btn add__btn'/></Link>
+    </section>
   )
 }
 
