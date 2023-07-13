4148e352d67d9dd996940e319403b4f4a7995e1c
Abiodun <abiodunhodonu@gmail.com>
2023-07-13 22:59:06 +0100

    create note page design

--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import { IoIosArrowBack } from 'react-icons/io'
 
 const CreateNote = () => {
   return (
-    <div>
-      GET shii
-    </div>
+    <section>
+      <header className="create-note__header">
+        <Link to="/" className='btn'><IoIosArrowBack /></Link>
+        <button className="btn lg primary">Save</button>
+      </header>
+      <form className="create-note__form">
+        <input type="text" placeholder='title...' autoFocus />
+        <textarea rows="30" placeholder='write everthing here'></textarea>
+      </form>
+    </section>
   )
 }
 
