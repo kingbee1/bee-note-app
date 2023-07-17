2cdff57f792aacd9cc447440d8b17531fd59f52a
Abiodun <abiodunhodonu@gmail.com>
2023-07-17 21:53:21 +0100

    edit function

--- src/pages/EditNote.jsx
+++ src/pages/EditNote.jsx
@@ -1,7 +1,8 @@
-import { Link, useParams } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import { IoIosArrowBack } from 'react-icons/io'
 import { RiDeleteBin6Line } from 'react-icons/ri'
 import { useState } from 'react';
+import useCreateDate from '../components/useCreateDate';
 
 
 const EditNote = ({notes, sn}) => {
@@ -13,18 +14,41 @@ const EditNote = ({notes, sn}) => {
   //then,
   const [title, setTitle] = useState(note.title)
   const [write, setWrite] = useState(note.write)
-  console.log({note})
+  //the date hook i created.
+  const date = useCreateDate();
+  const navigate = useNavigate();
+
+  const handleForm = (e) => {
+    e.preventDefault()
+    if (title && write) {
+      //spreaad the object itthe old note passed as prop, and get the title, write up et date.
+      const newNote = {...notes, title, write, date}
+
+      //now, map through.
+      const newNotes = notes.map(item, id => {
+        //if the item is same with unique id from url, then it will be the new note.
+        if (item.id == id) {
+          item = newNote
+        }
+        return item;
+      })
+      //to update note.
+      sn(newNotes)
+      navigate('/')
+    }
+  }
+
 
   return (
     <section>
       <header className="create-note__header">
         <Link to="/" className='btn'><IoIosArrowBack /></Link>
-        <button className="btn lg primary">Save</button>
+        <button className="btn lg primary"onClick={handleForm}>Save</button>
         <button className="btn lg danger"><RiDeleteBin6Line /> </button>
       </header>
-      <form className="create-note__form">
-        <input type="text" placeholder='title...' autoFocus />
-        <textarea rows="30" placeholder='write everthing here...'></textarea>
+      <form className="create-note__form" onSubmit={handleForm}>
+        <input type="text" placeholder='title...' autoFocus value={title} onChange={(e) => setTitle(e.target.value)} />
+        <textarea rows="30" placeholder='write everthing here...' value={write} onChange={(e) =>setWrite(e.target.value)}></textarea>
       </form>
     </section>
   )
