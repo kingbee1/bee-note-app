dadc6fcdf47cc519cc3700ad7ad7ac99f08807cd
Abiodun <abiodunhodonu@gmail.com>
2023-07-17 17:43:33 +0100

    created a date hook and imported to create page

--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -1,18 +1,24 @@
 import { Link } from 'react-router-dom'
 import { IoIosArrowBack } from 'react-icons/io'
 import { useState } from 'react'
+//npm i uuid. a package that creates unique id.
 import { v4 as uuid } from 'uuid'
+import useCreateDate from '../components/useCreateDate'
+
 
 const CreateNote = () => {
   const [title, setTitle] = useState('')
   const [write, setWrite] = useState('')
+  const date = useCreateDate()
+ 
 
   const handleSubmit = (e) => {
     e.preventDefault()
     //form validation to ensure if user actually enters a title writes note.
     if (title && write) {
-      console.log({title, write})
-
+      //a function that creates an id for all note object created. 
+      const note = {id: uuid(), title, write, date}
+      console.log({note})
     }
 
   }
