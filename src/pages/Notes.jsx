2fdbe3a788ae2ad80ff771f0013b3aaec79404d8
Abiodun <abiodunhodonu@gmail.com>
2023-07-18 16:11:24 +0100

    handled search function

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,28 +1,46 @@
 import { CiSearch } from 'react-icons/ci'
+import { MdClose } from 'react-icons/md'
 import { BsPlusLg } from 'react-icons/bs'
 //import dummyNotes from '../dummy_notes'
 import { Link } from 'react-router-dom'
 import NoteItem from '../components/NoteItem'
 import { v4 as uuid } from 'uuid'
 import { useState } from 'react'
+import { useEffect } from 'react'
 
 //accepting and destructuring the note props from App.js here.
 //.map is not a function again. back to debug.
 const Notes = ({ notes }) => {
   const [displaySearch, setDisplaySearch]= useState(false)
+  const [text, setText] = useState('')
+  //to filter notes. initially,the note from app.js, (from local storage).
+  const [filteredNote, setFilteredNote] = useState(notes)
+
+  const handleSearch = () => {
+    setFilteredNote(notes.filter(note => {
+      if(note.title.toLowerCase().match(text.toLowerCase())) {
+        return note;
+      }
+    }))
+  }
+
+  useEffect((handleSearch)=> {
+
+  }, [text])
+
 
   return (
     <section>
       <header className='notes__header'>
         {!displaySearch && <h2>Notes</h2>}
-        {displaySearch && <input type="text" autoFocus placeholder='Keyword...' />}
-        <button className='btn' onClick={()=>setDisplaySearch(!displaySearch)}><CiSearch /></button>
+        {displaySearch && <input type="text" autoFocus placeholder='Keyword...' value={text} onChange={(e)=>{setText(e.target.value);handleSearch()}} />}
+        <button className='btn' onClick={()=>setDisplaySearch(!displaySearch)}>{displaySearch ? <MdClose /> : <CiSearch />}</button>
       </header>
       <div className="notes__container" >
 
     {
       //note prop at the end.
-      notes.map(note => <NoteItem key={note.id} note={note}/>)
+      filteredNote.map(note => <NoteItem key={note.id} note={note}/>)
     }
       </div>
       <Link to={'/create'} className='btn add__btn'><BsPlusLg/></Link>
