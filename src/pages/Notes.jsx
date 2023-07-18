b544686d8a259355e03eb593e47eca59f793f873
Abiodun <abiodunhodonu@gmail.com>
2023-07-18 15:48:15 +0100

    toggle the search bar display

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,28 +1,28 @@
 import { CiSearch } from 'react-icons/ci'
 import { BsPlusLg } from 'react-icons/bs'
-import dummyNotes from '../dummy_notes'
+//import dummyNotes from '../dummy_notes'
 import { Link } from 'react-router-dom'
 import NoteItem from '../components/NoteItem'
+import { v4 as uuid } from 'uuid'
+import { useState } from 'react'
 
 //accepting and destructuring the note props from App.js here.
 //.map is not a function again. back to debug.
 const Notes = ({ notes }) => {
-
+  const [displaySearch, setDisplaySearch]= useState(false)
 
   return (
     <section>
       <header className='notes__header'>
-        <h2>Notes</h2>
-        {/* <input type="text" autoFocus placeholder='Keyword...' /> */}
-        <button className='btn'><CiSearch /></button>
+        {!displaySearch && <h2>Notes</h2>}
+        {displaySearch && <input type="text" autoFocus placeholder='Keyword...' />}
+        <button className='btn' onClick={()=>setDisplaySearch(!displaySearch)}><CiSearch /></button>
       </header>
-      <div className="notes__container">
+      <div className="notes__container" >
+
     {
-      
-      
       //note prop at the end.
       notes.map(note => <NoteItem key={note.id} note={note}/>)
-      
     }
       </div>
       <Link to={'/create'} className='btn add__btn'><BsPlusLg/></Link>
